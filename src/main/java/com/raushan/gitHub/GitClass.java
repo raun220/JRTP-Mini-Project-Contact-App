1c5b855c5e8dd04e657d2b616f5c9727038fc14b
Raushan Kumar <raun3220@gmail.com>
2023-08-02 22:50:45 +0530

    syntax error solved

--- src/main/java/com/raushan/gitHub/GitClass.java
+++ src/main/java/com/raushan/gitHub/GitClass.java
@@ -8,7 +8,7 @@ public class GitClass {
 	public void git() {
 	
 	System.out.println("Hi, learning Git and Git Hub");
-	System.out.println("anoter line added to check pull)";	
+	System.out.println("anoter line added to check pull");	
 		
 	}
 
