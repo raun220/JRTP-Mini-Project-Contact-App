3101e880640c575639f8be87a71278518e7125ce
Raushan Kumar <raun3220@gmail.com>
2023-08-02 22:45:27 +0530

    from actual folder

---
+++ src/main/java/com/raushan/gitHub/GitClass.java
@@ -0,0 +1,13 @@
+package com.raushan.gitHub;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GitClass {
+	
+	public void git() {
+	
+	System.out.println("Hi, learning Git and Git Hub");
+	}
+
+}
