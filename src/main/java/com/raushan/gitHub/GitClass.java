30fcb8bcfeebcb195fddb5f8bd57b856dc2a31d3
Raushan Kumar <raun3220@gmail.com>
2023-08-03 07:20:04 +0530

    bug 104 fixed

--- src/main/java/com/raushan/gitHub/GitClass.java
+++ src/main/java/com/raushan/gitHub/GitClass.java
@@ -9,6 +9,16 @@ public class GitClass {
 	
 	System.out.println("Hi, learning Git and Git Hub");
 	System.out.println("anoter line added to check pull");	
+	
+	//changes for Bug 101
+	int p =10;
+	int q =11;
+	int r = p+q;
+	
+	// bug fixed 104
+	System.out.println(r);
+	
+	
 		
 	}
 
